6cb0f3d6394897ebb34f0602787793c8a49f0953
Ubuntu <ubuntu@ip-10-160-30-40.ap-northeast-1.compute.internal>
2011-12-03 05:46:57 +0000

    change db user

--- settings.py
+++ settings.py
@@ -11,8 +11,8 @@ MANAGERS = ADMINS
 
 DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 DATABASE_NAME = 'showbox'             # Or path to database file if using sqlite3.
-DATABASE_USER = 'ecolemo'             # Not used with sqlite3.
-DATABASE_PASSWORD = 'ecolemo'         # Not used with sqlite3.
+DATABASE_USER = 'showbox'             # Not used with sqlite3.
+DATABASE_PASSWORD = 'showbox'         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 DATABASE_OPTIONS = {
